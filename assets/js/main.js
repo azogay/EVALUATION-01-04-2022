907aeeedf98d5e6280ac2d9515e57aec33d05ea7
hajar <hajarazogay@gmail.com>
2022-04-04 03:47:54 -0700

    mise a jour

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
+function getCookie(cName) {
+    const name = cName + "=";
+    const cDecoded = decodeURIComponent(document.cookie); //to be careful
+    const cArr = cDecoded.split('; ');
+    let res;
+    cArr.forEach(val => {
+        if (val.indexOf(name) === 0) res = val.substring(name.length);
+    })
+    return res
+}
 
+function showHideElement(idHtml) {
+    if (document.getElementById(idHtml).style.visibility == 'visible') {
+        document.getElementById(idHtml).style.visibility = 'hidden';
+    } else {
+        document.getElementById(idHtml).style.visibility = 'visible';
+    }
+}
+
+function ajouterAuPanier(idProduit) {
+}
+
+function supprimerDuPanier(idProduit) {
+}
+
+function viderPanier() {
+    document.cookie = "panier=; expires=" + Date() + "; path=/;";
+}
